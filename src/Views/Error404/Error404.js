32aa8cd761c427084034b88e1d83083216b27e19
Gilles Quodbach <quodbach.gilles@gmail.com>
2023-02-08 11:32:00 +0100

    Correction page 404 (responsive)

--- src/Views/Error404/Error404.js
+++ src/Views/Error404/Error404.js
@@ -7,7 +7,7 @@ export default function Error404() {
     <div className="error404-container">
       <h1 className="error404">404</h1>
       <p className="error-message">
-        Oups! La page que vous demandez n'existe pas.
+        Oups! La page que <br /> vous demandez n'existe pas.
       </p>
       <p>
         <Link to="/" className="back-to-home">
