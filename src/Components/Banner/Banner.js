ad325889e09d6676b4844b6ff7f88f306bb09bff
Gilles Quodbach <quodbach.gilles@gmail.com>
2023-02-09 15:38:18 +0100

    Corrections

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,16 +3,7 @@ import "./Banner.css";
 
 export default function Banner(props) {
   return (
-    <div
-      className={props.class}
-      // style={{
-      //   background: `linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${props.bannerBackground})`,
-      //   backgroundSize: "cover",
-      //   backgroundRepeat: "no-repeat",
-      //   backgroundPosition: "25% 50%",
-      //   // backgroundImage: `url(${props.bannerBackground})`,
-      // }}
-    >
+    <div className={props.class}>
       <p className="banner-text">Chez vous, partout et ailleurs</p>
     </div>
   );
